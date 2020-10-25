99dcb86c2478dd7700f8d0ae541c14786401fad1
SCP - 5 1 21 15 <mikhail.firsov.2001@mail.ru>
2020-10-25 03:14:08 +0300

    Добавлены новые тесты в CompositeFunctionTest.

--- src/test/java/ru/ssau/tk/kaf/kudrinandfirsov/functions/CompositeFunctionTest.java
+++ src/test/java/ru/ssau/tk/kaf/kudrinandfirsov/functions/CompositeFunctionTest.java
@@ -13,6 +13,8 @@ public class CompositeFunctionTest {
 
     private final CompositeFunction composite2 = new CompositeFunction(composite1, new SqrFunction());
 
+    private final CompositeFunction composite3 = new CompositeFunction(composite2, new ZeroFunction());
+
     @Test
     public void testFunction() {
         assertEquals(composite1.apply(50), 25, 0.000001);
@@ -21,5 +23,8 @@ public class CompositeFunctionTest {
         assertEquals(composite2.apply(10), 1, 0.000001);
         assertEquals(composite2.apply(20), 16, 0.00001);
         assertEquals(composite2.apply(-30), 81, 0.00001);
+        assertEquals(composite3.apply(66), 0, 0.00001);
+        assertEquals(composite3.apply(12), 0, 0.00001);
+        assertEquals(composite3.apply(87), 0, 0.00001);
     }
 }
