17fe9c9678fdc2efbdbb70c4ab1ce6d6ec04fa26
Kudrin <viktorkudrin5@gmail.com>
2020-11-27 23:46:15 +0400

    Теперь точно полный тест ArrayTabulatedFunctionTest

--- src/test/java/ru/ssau/tk/kaf/kudrinandfirsov/functions/ArrayTabulatedFunctionTest.java
+++ src/test/java/ru/ssau/tk/kaf/kudrinandfirsov/functions/ArrayTabulatedFunctionTest.java
@@ -15,11 +15,14 @@ public class ArrayTabulatedFunctionTest {
     private final double[] xValues1 = new double[]{5};
     private final double[] yValues1 = new double[]{10};
     ArrayTabulatedFunction arrayTabulatedUnitArrayFunction = new ArrayTabulatedFunction(xValues1, yValues1);
+    ArrayTabulatedFunction arrayTabulatedMathChangeFromToFunction = new ArrayTabulatedFunction(tenthFunction, 9, 1, 17);
 
     @Test
     public void testGetCount() {
         assertEquals(arrayTabulatedFunction.getCount(), 9, error);
+        assertEquals(arrayTabulatedMathFunction.getCount(), 17, error);
         assertEquals(arrayTabulatedUnitArrayFunction.getCount(), 1, error);
+        assertEquals(arrayTabulatedMathChangeFromToFunction.getCount(), 17, error);
     }
 
     @Test
@@ -31,6 +34,9 @@ public class ArrayTabulatedFunctionTest {
         assertEquals(arrayTabulatedMathFunction.getX(2), 2, error);
         assertEquals(arrayTabulatedMathFunction.getX(4), 3, error);
         assertEquals(arrayTabulatedUnitArrayFunction.getX(0), 5, error);
+        assertEquals(arrayTabulatedMathChangeFromToFunction.getX(0), 1, error);
+        assertEquals(arrayTabulatedMathChangeFromToFunction.getX(2), 2, error);
+        assertEquals(arrayTabulatedMathChangeFromToFunction.getX(4), 3, error);
     }
 
     @Test
@@ -42,16 +48,21 @@ public class ArrayTabulatedFunctionTest {
         assertEquals(arrayTabulatedMathFunction.getY(2), 0.2, error);
         assertEquals(arrayTabulatedMathFunction.getY(4), 0.3, error);
         assertEquals(arrayTabulatedUnitArrayFunction.getY(0), 10, error);
+        assertEquals(arrayTabulatedMathChangeFromToFunction.getY(0), 0.1, error);
+        assertEquals(arrayTabulatedMathChangeFromToFunction.getY(2), 0.2, error);
+        assertEquals(arrayTabulatedMathChangeFromToFunction.getY(4), 0.3, error);
     }
 
     @Test
     public void testSetY() {
         arrayTabulatedFunction.setY(0, -6);
-        arrayTabulatedMathFunction.setY(1, -6);
-        arrayTabulatedUnitArrayFunction.setY(0, 7);
         assertEquals(arrayTabulatedFunction.getY(0), -6, error);
+        arrayTabulatedMathFunction.setY(1, -6);
         assertEquals(arrayTabulatedMathFunction.getY(1), -6, error);
+        arrayTabulatedUnitArrayFunction.setY(0, 7);
         assertEquals(arrayTabulatedUnitArrayFunction.getY(0), 7, error);
+        arrayTabulatedMathChangeFromToFunction.setY(1, -6);
+        assertEquals(arrayTabulatedMathChangeFromToFunction.getY(1), -6, error);
     }
 
     @Test
@@ -59,6 +70,7 @@ public class ArrayTabulatedFunctionTest {
         assertEquals(arrayTabulatedFunction.leftBound(), -3, error);
         assertEquals(arrayTabulatedMathFunction.leftBound(), 1, error);
         assertEquals(arrayTabulatedUnitArrayFunction.leftBound(), 5, error);
+        assertEquals(arrayTabulatedMathChangeFromToFunction.leftBound(), 1, error);
     }
 
     @Test
@@ -66,6 +78,7 @@ public class ArrayTabulatedFunctionTest {
         assertEquals(arrayTabulatedFunction.rightBound(), 5, error);
         assertEquals(arrayTabulatedMathFunction.rightBound(), 9, error);
         assertEquals(arrayTabulatedUnitArrayFunction.rightBound(), 5, error);
+        assertEquals(arrayTabulatedMathChangeFromToFunction.rightBound(), 9, error);
     }
 
     @Test
@@ -79,6 +92,9 @@ public class ArrayTabulatedFunctionTest {
         assertEquals(arrayTabulatedUnitArrayFunction.indexOfX(5), 0, error);
         assertEquals(arrayTabulatedUnitArrayFunction.indexOfX(6), -1, error);
         assertEquals(arrayTabulatedUnitArrayFunction.indexOfX(4), -1, error);
+        assertEquals(arrayTabulatedMathChangeFromToFunction.indexOfX(2), 2, error);
+        assertEquals(arrayTabulatedMathChangeFromToFunction.indexOfX(4), 6, error);
+        assertEquals(arrayTabulatedMathChangeFromToFunction.indexOfX(20), -1, error);
     }
 
     @Test
@@ -92,6 +108,9 @@ public class ArrayTabulatedFunctionTest {
         assertEquals(arrayTabulatedUnitArrayFunction.indexOfY(10), 0, error);
         assertEquals(arrayTabulatedUnitArrayFunction.indexOfY(9), -1, error);
         assertEquals(arrayTabulatedUnitArrayFunction.indexOfY(11), -1, error);
+        assertEquals(arrayTabulatedMathChangeFromToFunction.indexOfY(0.2), 2, error);
+        assertEquals(arrayTabulatedMathChangeFromToFunction.indexOfY(0.4), 6, error);
+        assertEquals(arrayTabulatedMathChangeFromToFunction.indexOfY(2), -1, error);
     }
 
     @Test
@@ -110,6 +129,11 @@ public class ArrayTabulatedFunctionTest {
         assertEquals(arrayTabulatedUnitArrayFunction.floorIndexOfX(4.9), 0, error);
         assertEquals(arrayTabulatedUnitArrayFunction.floorIndexOfX(6), 0, error);
         assertEquals(arrayTabulatedUnitArrayFunction.floorIndexOfX(4), 0, error);
+        assertEquals(arrayTabulatedMathChangeFromToFunction.floorIndexOfX(1.6), 1, error);
+        assertEquals(arrayTabulatedMathChangeFromToFunction.floorIndexOfX(3.8), 5, error);
+        assertEquals(arrayTabulatedMathChangeFromToFunction.floorIndexOfX(10), 16, error);
+        assertEquals(arrayTabulatedMathChangeFromToFunction.floorIndexOfX(0), 0, error);
+        assertEquals(arrayTabulatedMathChangeFromToFunction.floorIndexOfX(1.1), 0, error);
     }
 
     @Test
@@ -117,6 +141,7 @@ public class ArrayTabulatedFunctionTest {
         assertEquals(arrayTabulatedFunction.extrapolateLeft(-4), -8, error);
         assertEquals(arrayTabulatedMathFunction.extrapolateLeft(-1), -0.1, error);
         assertEquals(arrayTabulatedUnitArrayFunction.extrapolateLeft(4), 10, error);
+        assertEquals(arrayTabulatedMathChangeFromToFunction.extrapolateLeft(-1), -0.1, error);
     }
 
     @Test
@@ -124,6 +149,7 @@ public class ArrayTabulatedFunctionTest {
         assertEquals(arrayTabulatedFunction.extrapolateRight(10), 20, error);
         assertEquals(arrayTabulatedMathFunction.extrapolateRight(10), 1, error);
         assertEquals(arrayTabulatedUnitArrayFunction.extrapolateRight(6), 10, error);
+        assertEquals(arrayTabulatedMathChangeFromToFunction.extrapolateRight(10), 1, error);
     }
 
     @Test
@@ -136,5 +162,8 @@ public class ArrayTabulatedFunctionTest {
         assertEquals(arrayTabulatedMathFunction.interpolate(10, arrayTabulatedMathFunction.floorIndexOfX(10)), 1, error);
         assertEquals(arrayTabulatedUnitArrayFunction.interpolate(4, 0), 10, error);
         assertEquals(arrayTabulatedUnitArrayFunction.interpolate(6, 0), 10, error);
+        assertEquals(arrayTabulatedMathChangeFromToFunction.interpolate(2.2, arrayTabulatedMathChangeFromToFunction.floorIndexOfX(2.2)), 0.22, error);
+        assertEquals(arrayTabulatedMathChangeFromToFunction.interpolate(-1, arrayTabulatedMathChangeFromToFunction.floorIndexOfX(-1)), -0.1, error);
+        assertEquals(arrayTabulatedMathChangeFromToFunction.interpolate(10, arrayTabulatedMathChangeFromToFunction.floorIndexOfX(10)), 1, error);
     }
 }
