c8214de6c66c68e0c4c7ccb378cae7dcb68f87f0
SCP - 5 1 21 15 <mikhail.firsov.2001@mail.ru>
2020-12-20 21:02:22 +0400

    Класс "листа".

---
+++ src/main/java/ru/ssau/tk/kaf/kudrinandfirsov/functions/LinkedListTabulatedFunction.java
@@ -0,0 +1,157 @@
+package ru.ssau.tk.kaf.kudrinandfirsov.functions;
+
+public class LinkedListTabulatedFunction extends AbstractTabulatedFunction {
+    private Node head;
+    protected int count;
+
+    protected static class Node {
+        public Node next;
+        public Node prev;
+        public double x;
+        public double y;
+    }
+
+    private void addNode(double x, double y) {
+        Node newNode = new Node();
+        newNode.x = x;
+        newNode.y = y;
+        if (head == null) {
+            head = newNode;
+            newNode.prev = newNode;
+            newNode.next = newNode;
+        } else {
+            Node last = head.prev;
+            newNode.prev = last;
+            newNode.next = head;
+            head.prev = newNode;
+            last.next = newNode;
+        }
+        count += 1;
+    }
+
+
+    public LinkedListTabulatedFunction(double[] xValues, double[] yValues) {
+        for (int i = 0; i < xValues.length; i++) {
+            this.addNode(xValues[i], yValues[i]);
+        }
+    }
+
+    public LinkedListTabulatedFunction(MathFunction source, double xFrom, double xTo, int count) {
+        this.count = count;
+        final double step = (xTo - xFrom) / (count - 1);
+        double a = xFrom;
+        for (int i = 0; i < count; i++) {
+            this.addNode(a, source.apply(a));
+            a += step;
+        }
+    }
+
+    @Override
+    public double leftBound() {
+        return head.x;
+    }
+
+    @Override
+    public double rightBound() {
+        return head.prev.x;
+    }
+
+    @Override
+    protected int floorIndexOfX(double x) {
+        if (x < head.x) {
+            throw new IllegalArgumentException("X is beyond the left border");
+        }
+        Node indexNode = head;
+        for (int i = 0; i < count; i++) {
+            if (indexNode.x < x) {
+                indexNode = indexNode.next;
+            } else {
+                if (i == 0) {
+                    return 0;
+                }
+                return i - 1;
+            }
+        }
+        return getCount();
+    }
+
+    @Override
+    protected double extrapolateRight(double x) {
+        if (head.x == head.prev.x) {
+            return head.x;
+        }
+        return interpolate(x, head.prev.prev.x, head.prev.x, head.prev.prev.y, head.prev.y);
+    }
+
+    @Override
+    protected double extrapolateLeft(double x) {
+        if (head.x == head.prev.x) {
+            return head.x;
+        }
+        return interpolate(x, head.x, head.next.x, head.y, head.next.y);
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    @Override
+    protected double interpolate(double x, int floorIndex) {
+        Node leftNode = getNode(floorIndex);
+        Node rightNode = leftNode.next;
+        return interpolate(x, leftNode.x, rightNode.x, leftNode.y, rightNode.y);
+    }
+
+    @Override
+    public int indexOfY(double y) {
+        Node indexNode = head;
+        for (int i = 0; i < count; i++) {
+            if (indexNode.y == y) {
+                return i;
+            }
+            indexNode = indexNode.next;
+        }
+        return -1;
+    }
+
+    @Override
+    public int indexOfX(double x) {
+        Node indexNode = head;
+        for (int i = 0; i < count; i++) {
+            if (indexNode.x == x) {
+                return i;
+            }
+            indexNode = indexNode.next;
+        }
+        return -1;
+    }
+
+    @Override
+    public double getX(int index) {
+        return getNode(index).x;
+    }
+
+    @Override
+    public double getY(int index) {
+        return getNode(index).y;
+    }
+
+    @Override
+    public void setY(int index, double value) {
+        getNode(index).y = value;
+    }
+
+    private Node getNode(int index) {
+        Node indexNode = head;
+        for (int i = 0; i < count; i++) {
+            if (i == index) {
+                return indexNode;
+            }
+            indexNode = indexNode.next;
+        }
+        return new Node();
+    }
+
+
+
+}
