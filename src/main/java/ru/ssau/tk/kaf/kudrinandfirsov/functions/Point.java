1a04507892e1325199f4205f13d20ddff51cd34c
Kudrin <viktorkudrin5@gmail.com>
2021-01-11 18:08:18 +0400

    сощдан класс point

---
+++ src/main/java/ru/ssau/tk/kaf/kudrinandfirsov/functions/Point.java
@@ -0,0 +1,12 @@
+package ru.ssau.tk.kaf.kudrinandfirsov.functions;
+
+public class Point {
+
+    final public double x;
+    final public double y;
+
+    public Point (double x, double y){
+        this.x=x;
+        this.y=y;
+    }
+}
