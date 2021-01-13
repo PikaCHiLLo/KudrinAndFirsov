899298116ae0a3b4ab4f2f8c87eaf29d5c37104b
Kudrin <viktorkudrin5@gmail.com>
2021-01-13 05:57:32 +0400

    Дополнен TabulatedFunctionOperationService, теперь он так кипитярить может, ю ноу блин?

--- src/main/java/ru/ssau/tk/kaf/kudrinandfirsov/operations/TabulatedFunctionOperationService.java
+++ src/main/java/ru/ssau/tk/kaf/kudrinandfirsov/operations/TabulatedFunctionOperationService.java
@@ -2,8 +2,29 @@ package ru.ssau.tk.kaf.kudrinandfirsov.operations;
 
 import ru.ssau.tk.kaf.kudrinandfirsov.functions.TabulatedFunction;
 import ru.ssau.tk.kaf.kudrinandfirsov.functions.Point;
+import ru.ssau.tk.kaf.kudrinandfirsov.exceptions.InconsistentFunctionsException;
+import ru.ssau.tk.kaf.kudrinandfirsov.functions.factory.ArrayTabulatedFunctionFactory;
+import ru.ssau.tk.kaf.kudrinandfirsov.functions.factory.TabulatedFunctionFactory;
 
 public class TabulatedFunctionOperationService {
+    private TabulatedFunctionFactory factory;
+
+    public TabulatedFunctionOperationService(TabulatedFunctionFactory factory) {
+        this.factory = factory;
+    }
+
+    public TabulatedFunctionOperationService() {
+        this.factory = new ArrayTabulatedFunctionFactory();
+    }
+
+    public TabulatedFunctionFactory getFactory() {
+        return factory;
+    }
+
+    public void setFactory(TabulatedFunctionFactory factory) {
+        this.factory = factory;
+    }
+
     public static Point[] asPoints(TabulatedFunction tabulatedFunction) {
         int i = 0;
         Point[] dots = new Point[tabulatedFunction.getCount()];
@@ -13,4 +34,36 @@ public class TabulatedFunctionOperationService {
         return dots;
     }
 
+    private interface BiOperation {
+        double apply(double u, double v);
+    }
+
+    private TabulatedFunction doOperation(TabulatedFunction a, TabulatedFunction b, BiOperation operation) {
+
+        if (a.getCount() != b.getCount()) {
+            throw new InconsistentFunctionsException("Количества записей в табулированных функциях не совпадают.");
+        }
+
+        Point[] aDots = asPoints(a);
+        Point[] bDots = asPoints(b);
+        double[] xValues = new double[a.getCount()];
+        double[] yValues = new double[b.getCount()];
+
+        for (int i = 0; i < a.getCount(); i++) {
+            if (aDots[i].x != bDots[i].x) {
+                throw new InconsistentFunctionsException("Координаты x двух функций разные.");
+            }
+            xValues[i] = aDots[i].x;
+            yValues[i] = operation.apply(aDots[i].y, bDots[i].y);
+        }
+        return factory.create(xValues, yValues);
+    }
+
+    public TabulatedFunction sum(TabulatedFunction a, TabulatedFunction b) {
+        return doOperation(a, b, (u, v) -> u + v);
+    }
+
+    public TabulatedFunction subtract(TabulatedFunction a, TabulatedFunction b) {
+        return doOperation(a, b, (u, v) -> u - v);
+    }
 }
