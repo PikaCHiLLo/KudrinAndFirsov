749ac97da30e6170d6e072fffa10d9669ffc322c
Kudrin <viktorkudrin5@gmail.com>
2021-01-13 19:26:43 +0400

    добавлен метод writeTabulatedFunction который записывает функции в врайтер, и добавлен класс TabulatedFunctionFileWriter,который делает запись в файлы, не сразу получилось записать, но получилось

--- src/main/java/ru/ssau/tk/kaf/kudrinandfirsov/io/FunctionsIO.java
+++ src/main/java/ru/ssau/tk/kaf/kudrinandfirsov/io/FunctionsIO.java
@@ -1,7 +1,21 @@
 package ru.ssau.tk.kaf.kudrinandfirsov.io;
 
+import ru.ssau.tk.kaf.kudrinandfirsov.functions.Point;
+import ru.ssau.tk.kaf.kudrinandfirsov.functions.TabulatedFunction;
+
+import java.io.*;
+
 final public class FunctionsIO {
     private FunctionsIO() {
         throw new UnsupportedOperationException();
+    }
+
+    static void writeTabulatedFunction(BufferedWriter writer, TabulatedFunction function){
+        PrintWriter printWriter = new PrintWriter(writer);
+        printWriter.println(function.getCount());
+        for (Point temp : function) {
+            printWriter.printf("%f %f\n", temp.x, temp.y);
+        }
+        printWriter.flush();
     }
 }
