dd97c9596dab34c2de31c3f4251a221aeccda37d
Kudrin <viktorkudrin5@gmail.com>
2021-02-19 02:53:06 +0400

    испытание многопоточности, и я пошел дальше лабы, и получилось немного забавно, ведь у меня могло начать поднимать игрик сразу 5 потоков, до переноса строки

---
+++ src/main/java/ru/ssau/tk/kaf/kudrinandfirsov/concurrent/ReadWriteTask.java
@@ -0,0 +1,25 @@
+package ru.ssau.tk.kaf.kudrinandfirsov.concurrent;
+
+import ru.ssau.tk.kaf.kudrinandfirsov.functions.TabulatedFunction;
+
+public class ReadWriteTask implements Runnable {
+    private TabulatedFunction tabulatedFunction;
+    public double x;
+    public double y;
+
+    ReadWriteTask(TabulatedFunction tabulatedFunction) {
+        this.tabulatedFunction = tabulatedFunction;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < tabulatedFunction.getCount(); i++) {
+            x = tabulatedFunction.getX(i);
+            y = tabulatedFunction.getY(i);
+            System.out.printf( "%s, before write: i =  % d  , x = %f, y = %f ", tabulatedFunction.getClass(), i, x, y);
+            tabulatedFunction.setY(i, y + 1);
+            y = tabulatedFunction.getY(i);
+            System.out.printf("%s, after write: i = %d, x = %f, y = %f \n", tabulatedFunction.getClass(), i, x, y);
+        }
+    }
+}
---
+++ src/main/java/ru/ssau/tk/kaf/kudrinandfirsov/concurrent/ReadWriteTaskExecutor.java
@@ -0,0 +1,23 @@
+package ru.ssau.tk.kaf.kudrinandfirsov.concurrent;
+
+import ru.ssau.tk.kaf.kudrinandfirsov.functions.TabulatedFunction;
+import ru.ssau.tk.kaf.kudrinandfirsov.functions.LinkedListTabulatedFunction;
+import ru.ssau.tk.kaf.kudrinandfirsov.functions.ZeroFunction;
+
+import java.util.ArrayList;
+
+public class ReadWriteTaskExecutor extends Thread {
+    public static void main(String[] args) throws InterruptedException {
+        TabulatedFunction linkedListTabulatedFunction = new LinkedListTabulatedFunction(new ZeroFunction(), 1, 10, 10);
+        ArrayList<Thread> listThread = new ArrayList<>();
+
+        for (int i = 0; i != 20; i++) {
+            listThread.add(new Thread(new ReadWriteTask(linkedListTabulatedFunction)));
+        }
+        for (Thread thread: listThread){
+            thread.start();
+        }
+        Thread.sleep(2000);
+        System.out.println(linkedListTabulatedFunction.toString());
+    }
+}
