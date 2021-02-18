c1190654f8114932800700fd44bc4880eb605cdb
Kudrin <viktorkudrin5@gmail.com>
2021-02-19 03:07:49 +0400

    теперь потоки записываются нормально и последовательно, а то фигня была

--- src/main/java/ru/ssau/tk/kaf/kudrinandfirsov/concurrent/ReadWriteTask.java
+++ src/main/java/ru/ssau/tk/kaf/kudrinandfirsov/concurrent/ReadWriteTask.java
@@ -3,7 +3,7 @@ package ru.ssau.tk.kaf.kudrinandfirsov.concurrent;
 import ru.ssau.tk.kaf.kudrinandfirsov.functions.TabulatedFunction;
 
 public class ReadWriteTask implements Runnable {
-    private TabulatedFunction tabulatedFunction;
+    private final TabulatedFunction tabulatedFunction;
     public double x;
     public double y;
 
@@ -15,10 +15,13 @@ public class ReadWriteTask implements Runnable {
     public void run() {
         for (int i = 0; i < tabulatedFunction.getCount(); i++) {
             x = tabulatedFunction.getX(i);
-            y = tabulatedFunction.getY(i);
-            System.out.printf( "%s, before write: i =  % d  , x = %f, y = %f ", Thread.currentThread().getName(), i, x, y);
-            tabulatedFunction.setY(i, y + 1);
-            y = tabulatedFunction.getY(i);
+            synchronized (tabulatedFunction) {
+                y = tabulatedFunction.getY(i);
+                System.out.printf("%s, before write: i =  % d  , x = %f, y = %f", Thread.currentThread().getName(), i, x, y);
+                System.out.println();
+                tabulatedFunction.setY(i, y + 1);
+                y = tabulatedFunction.getY(i);
+            }
             System.out.printf("%s, after write: i = %d, x = %f, y = %f \n", Thread.currentThread().getName(), i, x, y);
         }
     }
