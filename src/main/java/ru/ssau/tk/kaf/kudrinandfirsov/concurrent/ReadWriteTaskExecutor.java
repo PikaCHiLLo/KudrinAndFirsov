4f67464bdd36683683527746e7468ffd1290d19a
Kudrin <viktorkudrin5@gmail.com>
2021-02-20 00:57:50 +0400

    поправка

--- src/main/java/ru/ssau/tk/kaf/kudrinandfirsov/concurrent/ReadWriteTaskExecutor.java
+++ src/main/java/ru/ssau/tk/kaf/kudrinandfirsov/concurrent/ReadWriteTaskExecutor.java
@@ -5,11 +5,12 @@ import ru.ssau.tk.kaf.kudrinandfirsov.functions.LinkedListTabulatedFunction;
 import ru.ssau.tk.kaf.kudrinandfirsov.functions.ZeroFunction;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class ReadWriteTaskExecutor extends Thread {
     public static void main(String[] args) throws InterruptedException {
         TabulatedFunction linkedListTabulatedFunction = new LinkedListTabulatedFunction(new ZeroFunction(), 1, 10, 10);
-        ArrayList<Thread> listThread = new ArrayList<>();
+        List<Thread> listThread = new ArrayList<>();
 
         for (int i = 0; i != 20; i++) {
             listThread.add(new Thread(new ReadWriteTask(linkedListTabulatedFunction)));
